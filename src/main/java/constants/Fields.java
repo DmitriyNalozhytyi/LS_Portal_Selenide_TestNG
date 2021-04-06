78b831b55afcf2fca1e0911c31a6c8024ad2871e
lnick <lutsuk.nikolay@gmail.com>
2021-04-06 19:52:24 +0300

    Hot_Fixes

--- src/main/java/constants/Fields.java
+++ src/main/java/constants/Fields.java
@@ -12,6 +12,6 @@ public interface Fields {
     SelenideElement VACANCY_CITY                = $("#mat-select-1");
     SelenideElement VACANCY_FUNCTION            = $("#mat-select-2");
     SelenideElement VACANCY_SCHEDULE            = $("#mat-select-3");
-    SelenideElement JOB_APPLICANT_PHONE         = $(".main-input.vacancy-input.ng-pristine.ng-invalid");
-    SelenideElement ACCOMPANYING_TEXT           = $("#mceu_2").find("iframe");
+    SelenideElement JOB_APPLICANT_PHONE         = $(".form-element.form-element__first.vacancy-field").find("input");
+    SelenideElement ACCOMPANYING_TEXT           = $(".mce-tinymce.mce-container.mce-panel").find("iframe");
 }
